ca2d2111715290c0ce7f18c5af2fb7227fb2c8cc
Julia <grazia.julia@gmail.com>
2026-03-14 09:47:15 +0100

    PD-2 add unhappy paths, extend test cases and test number types

--- src/tests/smoke/test_shape1.cpp
+++ src/tests/smoke/test_shape1.cpp
@@ -29,28 +29,105 @@ TEST(test_shape1, AddFunction)
     ASSERT_EQ(shape.get(), nullptr);
 }
 
-TEST(test_torus, Compute)
+using TorusTypes = ::testing::Types<float, double, long double>;
+
+template <typename T> class TorusTypedTest : public ::testing::Test {};
+
+TYPED_TEST_SUITE(TorusTypedTest, TorusTypes);
+
+TYPED_TEST(TorusTypedTest, ComputeCorrectValues)
 {
-    ShapeParam<double> param;
+    ShapeParam<TypeParam> param;
 
-    param.set_attrib(PARAM_RADIUS, 5.0);
-    param.set_attrib(PARAM_RADIUS_2, 2.0);
+    param.set_attrib(PARAM_RADIUS, static_cast<TypeParam>(5));
+    param.set_attrib(PARAM_RADIUS_2, static_cast<TypeParam>(2));
 
-    Torus<double> torus(param);
+    Torus<TypeParam> torus(param);
 
-    ShapeResult<double> result = torus.compute();
+    ShapeResult<TypeParam> result = torus.compute();
 
-    double volume = result.get_attrib(RESULT_VOLUME);
-    double surface = result.get_attrib(RESULT_SURFACE);
+    TypeParam volume = result.get_attrib(RESULT_VOLUME);
+    TypeParam surface = result.get_attrib(RESULT_SURFACE);
 
-    const double PI = M_PI;
+    const TypeParam PI = static_cast<TypeParam>(M_PI);
 
-    double expected_volume = 2 * PI * PI * 5.0 * 2.0 * 2.0;
-    double expected_surface = 4 * PI * PI * 5.0 * 2.0;
+    TypeParam expected_volume = static_cast<TypeParam>(2) * PI * PI
+        * static_cast<TypeParam>(5) * static_cast<TypeParam>(2)
+        * static_cast<TypeParam>(2);
 
-    ASSERT_NEAR(volume, expected_volume, 1e-6);
-    ASSERT_NEAR(surface, expected_surface, 1e-6);
+    TypeParam expected_surface = static_cast<TypeParam>(4) * PI * PI
+        * static_cast<TypeParam>(5) * static_cast<TypeParam>(2);
+
+    ASSERT_NEAR(static_cast<double>(volume),
+                static_cast<double>(expected_volume), 1e-6);
+
+    ASSERT_NEAR(static_cast<double>(surface),
+                static_cast<double>(expected_surface), 1e-6);
 }
+
+TYPED_TEST(TorusTypedTest, ZeroTubeRadius)
+{
+    ShapeParam<TypeParam> param;
+
+    param.set_attrib(PARAM_RADIUS, static_cast<TypeParam>(5));
+    param.set_attrib(PARAM_RADIUS_2, static_cast<TypeParam>(0));
+
+    Torus<TypeParam> torus(param);
+
+    ShapeResult<TypeParam> result = torus.compute();
+
+    ASSERT_EQ(result.get_attrib(RESULT_VOLUME), static_cast<TypeParam>(0));
+    ASSERT_EQ(result.get_attrib(RESULT_SURFACE), static_cast<TypeParam>(0));
+}
+
+TYPED_TEST(TorusTypedTest, LargeValuesOverflow)
+{
+    ShapeParam<TypeParam> param;
+
+    param.set_attrib(PARAM_RADIUS, std::numeric_limits<TypeParam>::max());
+    param.set_attrib(PARAM_RADIUS_2, static_cast<TypeParam>(2));
+
+    Torus<TypeParam> torus(param);
+
+    ASSERT_THROW(torus.compute(), std::overflow_error);
+}
+
+TYPED_TEST(TorusTypedTest, NegativeRadius)
+{
+    ShapeParam<TypeParam> param;
+
+    param.set_attrib(PARAM_RADIUS, static_cast<TypeParam>(-5));
+    param.set_attrib(PARAM_RADIUS_2, static_cast<TypeParam>(2));
+
+    Torus<TypeParam> torus(param);
+
+    ASSERT_THROW(torus.compute(), std::invalid_argument);
+}
+
+TYPED_TEST(TorusTypedTest, NegativeTubeRadius)
+{
+    ShapeParam<TypeParam> param;
+
+    param.set_attrib(PARAM_RADIUS, static_cast<TypeParam>(5));
+    param.set_attrib(PARAM_RADIUS_2, static_cast<TypeParam>(-2));
+
+    Torus<TypeParam> torus(param);
+
+    ASSERT_THROW(torus.compute(), std::invalid_argument);
+}
+
+TYPED_TEST(TorusTypedTest, InvalidTorusGeometry)
+{
+    ShapeParam<TypeParam> param;
+
+    param.set_attrib(PARAM_RADIUS, static_cast<TypeParam>(5));
+    param.set_attrib(PARAM_RADIUS_2, static_cast<TypeParam>(5));
+
+    Torus<TypeParam> torus(param);
+
+    ASSERT_THROW(torus.compute(), std::invalid_argument);
+}
+
 
 TEST(test_torus, PrintContainsValues)
 {
@@ -67,7 +144,6 @@ TEST(test_torus, PrintContainsValues)
     ASSERT_NE(text.find("5"), std::string::npos);
     ASSERT_NE(text.find("2"), std::string::npos);
 }
-
 
 int main(int argc, char** argv)
 {
